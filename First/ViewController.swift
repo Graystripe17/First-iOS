02e5ba9606c5bce436e59e2b9fa529300176b3e0
Winston Van <kitty@winstons-macbook-pro.local>
2016-09-30 22:48:42 -0500

    Finished 'Connect UI to the Code'

--- First/ViewController.swift
+++ First/ViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var instruction: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the textfields user inputs through delegate callbacks
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        instruction.text = textField.text
+    }
+    
+    // MARK: Actions
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        nameTextField.text = "Woohoo!"
     }
 
 
